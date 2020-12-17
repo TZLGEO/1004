68aedcdb0431b8c2c367e983b44892cb3b198eee
TZLGEO <71419633+TZLGEO@users.noreply.github.com>
2020-12-16 21:23:19 -0500

    JSupdate

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,35 @@
 // Define SVG area dimensions
 var svgWidth = 960;
 // Load data from csv
-d3.json("/chartstay").then(function(chartData) {
+d3.json("/chartstay").then(function (chartData) {
 
   console.log(chartData)
 
-  //   weekPos = [1, 4, 7, 9]
-  //   peakPos = [2, 5, 8, 2]
-    
-  //   var trace1 = {
-  //     type: "scatter",
-  //     mode: "lines",
-  //     name: "my plot",
-  //     x: weekPos,
-  //     y: peakPos,
-  //     line: {
-  //       color: "#17BECF"
-  //     }
-  //   };
 
-  //   var data = [trace1];
 
-  //   Plotly.newPlot("plot", data)
+  let trace1 = {
+    type: "scatter",
+    mode: "lines",
+    name: "my plot",
+    x: [],
+    y: [],
+    line: {
+      color: "#17BECF"
+    }
+  };
+  chartData.foreach(function (val) {
+    trace1.x.push(val["x_vals"]);
+    trace1.x.push(val["y_vals"])
+  })
+
+  let layout = {
+    title: "plot",
+    yaxis: { title: "y axis" },
+    xaxis: { title: "y axis" }
+  }
+  var data = [trace1];
+
+  Plotly.newPlot("plot", data, layout)
   // 
+
 })
